f2d247d11e61bcfe63d53468b34d4206e818fa22
Josh Yap <joshua.c.yap@gmail.com>
2017-04-22 17:10:50 -0500

    uploading hw to github

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends = [{
+  "routeName": "josh",
+  "name":"Josh",
+  "photo":"http://orig04.deviantart.net/928c/f/2016/120/9/1/star_fox_zero___the_battle_begins___fox_mccloud_by_cosmic_phoenyx-da0t8h4.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}, {
+  "routeName": "matt",
+  "name":"Matt",
+  "photo":"http://orig09.deviantart.net/14e6/f/2008/153/4/7/falco_lombardi_by_slimthrowed.png",
+  "scores":[
+     3,
+     2,
+     3,
+     5,
+     4,
+     5,
+     1,
+     1,
+     2,
+     1
+   ]
+}, {
+  "routeName": "annie",
+  "name":"Annie",
+  "photo":"http://vignette2.wikia.nocookie.net/scratchpad/images/6/65/SlippyToad.png/revision/latest?cb=20120327114201",
+  "scores":[
+     1,
+     5,
+     1,
+     5,
+     1,
+     5,
+     3,
+     3,
+     3,
+     3
+   ]
+}];
+
+module.exports = friends;
